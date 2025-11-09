a3cfacefff3587d08bda13dfe3c89c83400760da
Mandar123454 <mandarkajbaje@gmail.com>
2025-11-09 23:36:06 +0530

    Add scripts package __init__ to fix pytest import error

---
+++ scripts/__init__.py
@@ -0,0 +1,15 @@
+from .clean_and_analyze import (
+    compute_basic_stats,
+    compute_nps,
+    find_candidate_columns,
+    anonymize,
+    load_data,
+)
+
+__all__ = [
+    "compute_basic_stats",
+    "compute_nps",
+    "find_candidate_columns",
+    "anonymize",
+    "load_data",
+]
